b8451104e07552c556875a36f978ee4a9bfd7ed2
edelgado-git <enriquedelgado806@gmail.com>
2022-06-01 14:11:24 -0400

    Added react query to the dashbaord

--- src/hooks/cars.hooks.ts
+++ src/hooks/cars.hooks.ts
@@ -3,13 +3,27 @@ import * as api from "../components/apiService";
 import { FormValues } from "../routes/registration/RegisterCar";
 import { toast } from "react-toastify";
 import { UseFormReset } from "react-hook-form";
+import { TrueLiteral } from "typescript";
 
 export enum ServerStateKeyEnum {
-    Cars = 'cars'
+    Cars = 'cars',
+    CarStats = 'carStats'
 }
 
 export const useGetCars = () => {
-   return useQuery(ServerStateKeyEnum.Cars, api.findCars);
+   return useQuery(ServerStateKeyEnum.Cars, async () => {
+    const { data } = await api.findCars();
+    return data;
+  });
+}
+
+export const useGetCarStats = (car: any) => {
+    return useQuery([ServerStateKeyEnum.CarStats, car], async () => {
+        const { data } = await api.findCarStats(car);
+        return data;
+      }, {
+        enabled: car !== null
+    });
 }
 
 export const useRegisterCar = (reset: UseFormReset<FormValues>) => {
---
+++ src/routes/dashboard/Home.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import HistoryTable from "./HistoryTable";
+import Dropdown from "../../components/Dropdown";
+
+import * as api from "../../components/apiService";
+
+import { useGetCars } from "../../hooks/cars.hooks";
+
+import { useQuery } from "react-query";
+
+let car: any = null;
+
+function Home() {
+  const { data: cars, isLoading } = useGetCars();
+  
+  //TODO: this is temporary because useGetCars will return an object with the totalCars and the array of cars
+  // const enabledStatsCall = data?.cars  //and the in enabled: !!enabledStatsCall
+  if (cars) {
+    car = cars[0]
+  }
+
+  const { data: carStats, refetch } = useQuery(
+    ["carStats", car],
+    async () => {
+      const { data } = await api.findCarStats(car);
+      return data;
+    },
+    {
+      enabled: !isLoading,
+    }
+  );
+
+  const findCarStats = (selectedCar: any) => {
+    car = selectedCar;
+
+    refetch();
+  }
+ 
+  return (
+    <>
+      <div style={{ width: "90%", margin: "auto", marginTop: "50px" }}>
+        {cars && cars.length > 0 ? (
+          <>
+            <div style={{ marginBottom: "20px", textAlign: "right" }}>
+              <Dropdown cars={cars} handleSelectedCar={findCarStats} /> 
+            </div>
+
+            {carStats && <HistoryTable carStats={carStats} />} 
+          </>
+        ) : !isLoading ? (
+          <div
+            style={{
+              margin: "auto",
+              width: "80%",
+              textAlign: "center",
+              marginTop: "250px",
+            }}
+          >
+            No registered cars found. Go to the Register Car tab to register a
+            car.
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
+}
+
+export default Home;
